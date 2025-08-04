fd250acf6e7e30c55e630910dc027c662e25257d
richard tsang <richard.tsang9641@gmail.com>
2025-08-05 00:41:57 +0800

    modify home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,23 @@
 'use client'
 import PageTransitionWrapper from "@/components/motionWrapper/PageTransitionWrapper";
 import { IQuestion } from "@/lib/definition";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [questions, setQuestions] = useState<IQuestion[]>([]);
-  useEffect(() => {
-    (async () => {
-      try {
-        const response = await fetch((process.env.NEXT_PUBLIC_API_URI || 'http://localhost:3005') + '/api/questions')
-        if (!response.ok) {
-          throw new Error(`Response status: ${response.status}`);
-        }
-        const json = await response.json();
-        setQuestions(json);
-      } catch (error) {
-
-      }
-    })()
-
-
-  }, [])
-
+  const level = ['easy', 'intermediate', 'hard'];
   return (
-    <main className=' w-full h-full flex flex-col flex-1 gap-6 '>
+    <main className='w-full h-full flex-1 py-6'>
       <PageTransitionWrapper>
-        <h1 className=" font-bold text-2xl">Questions</h1>
-        <h3 className="text-lg uppercase font-semibold">jumbled sentences</h3>
-        <ul className="flex flex-col gap-2">{questions.length > 0 ? questions.map((q: any, i) => <li key={i} className="flex gap-2 items-center justify-between"><p>#{i + 1}</p><p className="text-left flex-1">{q.question}</p><span className={`p-2 rounded-sm text-black bg-white bg-${q.level}`}>{q.level}</span></li>) : <p>loading...</p>}</ul>
+        <div className="flex flex-col gap-6">
+          <p className=" font-bold text-2xl">Welcome to playground!</p>
+          <p className="text-lg capitalize font-semibold">Game: jumbled sentences</p>
+          <div className="grid grid-cols-2 gap-9">
+            {level.length > 0 && level.map((l, i) =>
+              <Link key={l + i} className="p-6 h-30 rounded-sm bg-neutral hover:bg-accent hover:text-textPrimary! select-none transition-all " href={`/game/${l}`}><h3 className="text-lg sm:text-4xl font-semibold capitalize">Level {l}</h3></Link>
+            )}
+          </div>
+        </div>
       </PageTransitionWrapper>
     </main>
   );
