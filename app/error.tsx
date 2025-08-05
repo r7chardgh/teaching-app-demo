72c736c96300732bfae1a5243801e479f79634fa
richard tsang <richard.tsang9641@gmail.com>
2025-08-05 20:02:53 +0800

    add error page

---
+++ app/error.tsx
@@ -0,0 +1,31 @@
+'use client' // Error boundaries must be Client Components
+
+import { useEffect } from 'react'
+
+export default function Error({
+    error,
+    reset,
+}: {
+    error: Error & { digest?: string }
+    reset: () => void
+}) {
+    useEffect(() => {
+        // Log the error to an error reporting service
+        console.error(error)
+    }, [error])
+
+    return (
+        <div className='flex flex-col gap-4'>
+            <h2 className='text-lg'>Something went wrong!</h2>
+            <button
+                className='cursor-pointer p-2 rounded-2xl bg-neutral active:bg-textSecondary'
+                onClick={
+                    // Attempt to recover by trying to re-render the segment
+                    () => reset()
+                }
+            >
+                Try again
+            </button>
+        </div>
+    )
+}
