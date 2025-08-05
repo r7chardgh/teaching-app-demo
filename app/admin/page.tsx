4d48938393ab6c53b398d84c5e424dffca475212
richard tsang <richard.tsang9641@gmail.com>
2025-08-05 20:05:02 +0800

    modify admin & game page - fetch logic changed + chached

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,40 +1,36 @@
 'use client'
 import PageTransitionWrapper from '@/components/motionWrapper/PageTransitionWrapper';
-import React, { useActionState, useState } from 'react'
+import { IAdminFormState } from '@/lib/definition';
+import { createQuestion } from '@/lib/fetch';
+import React, { useActionState, useEffect, useState } from 'react'
 
+const initialState: IAdminFormState = {
+    message: '',
+    errors: {},
+    success: false,
+};
 const AdminPage = () => {
     const [newQuestion, setNewQuestion] = useState<string>('');
     const [level, setLevel] = useState<string>('easy');
-    const [message, handleSubmit, isPending] = useActionState(addQuestionToDB, newQuestion, ''); //main dish
-    async function addQuestionToDB(prevState: string, queryData: any) {
-        if (newQuestion.length > 0) {
-            try {
-                const res = await fetch((process.env.NEXT_PUBLIC_API_URI || 'http://localhost:3005') + '/api/questions', {
-                    method: 'post',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({ "question": newQuestion, "level": level })
-                })
-                if (res.status === 400) { throw new Error("Something went wrong!"); }
-                setNewQuestion('');
-                return "submitted";
-            } catch (error) {
-                return 'submit failed'
-            }
-        }
-        return 'no question is added, enter your question on the textfield please'
-    }
+    const [state, formAction, isPending] = useActionState(createQuestion, initialState);
+
+    useEffect(() => {
+      if(state.success){
+        setNewQuestion('')
+      }
+    }, [state.success])
+
     return (
         <main className=' w-full h-full flex-1 py-6'>
             <PageTransitionWrapper>
                 <div className='flex flex-col gap-6'>
                     <h1 className=' font-bold text-xl'>Admin Page</h1>
-                    <form action={handleSubmit} className='flex flex-col gap-6 m-auto w-full sm:w-[600px]'>
+                    <form action={formAction} className='flex flex-col gap-6 m-auto w-full sm:w-[600px]'>
                         <div className='flex flex-col gap-4'>
                             <div className='flex flex-col gap-2'>
                                 <label className='' htmlFor='newQuestion'>New Question:</label>
-                                <input required className='border-1 p-2 rounded-xl focus:text-accent' placeholder='enter the new question' id='newQuestion' type='text' value={newQuestion} onChange={(e) => setNewQuestion(e.target.value)} />
+                                <input required className='border-1 p-2 rounded-xl focus:text-accent' placeholder='enter the new question' name='question' id='newQuestion' type='text' value={newQuestion} onChange={(e) => setNewQuestion(e.target.value)} />
+                                {state.errors?.question && <p className='text-red-500'>{state.errors.question}</p>}
                             </div>
                             <div className='flex flex-col gap-2'>
 
@@ -44,10 +40,11 @@ const AdminPage = () => {
                                     <option value="intermediate">intermediate</option>
                                     <option value="hard">hard</option>
                                 </select>
+                                {state.errors?.level && <p className='text-red-500'>{state.errors.level}</p>}
                             </div>
                         </div>
-                        <button type="submit" className='px-4 py-3 bg-accent rounded-lg cursor-pointer'>add new question</button>
-                        {isPending ? <p>submitting...</p> : message}
+                        <button disabled={isPending} type="submit" className='px-4 py-3 bg-accent rounded-lg cursor-pointer'>{isPending ? 'submitting...' : 'add new question'}</button>
+                        {state.success && <p aria-live='polite'>submitted</p>}
                     </form>
                 </div>
             </PageTransitionWrapper>
